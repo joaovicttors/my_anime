b3f17ce7e45dac2c077c93b3ccb17b26a3f47c33
joaovicttors <joaovicttorrs@gmail.com>
2022-05-04 20:45:10 -0300

    add anime remote data source with your unit test

--- features/anime/src/main/java/com/joaovicttors/anime/data/models/AnimeRemoteModel.kt
+++ features/anime/src/main/java/com/joaovicttors/anime/data/models/AnimeRemoteModel.kt
@@ -1,7 +1,25 @@
-package com.joaovicttors.anime.data.models
+package com.joaovicttors.anime.data.models.remote
 
+import com.google.gson.annotations.SerializedName
 import com.joaovicttors.base.BaseModel
 
 data class AnimeRemoteModel(
-    val id: Int
-) : BaseModel()
+    @SerializedName(ID) val id: Int,
+    @SerializedName(FORMAT) val format: Int,
+    @SerializedName(STATUS) val status: Int,
+    @SerializedName(TITLES) val titles: Map<String, String>,
+    @SerializedName(DESCRIPTIONS) val descriptions: Map<String, String>,
+    @SerializedName(COVER_IMAGE) val coverImage: String,
+    @SerializedName(SCORE) val score: Float
+) : BaseModel() {
+
+    private companion object {
+        private const val ID: String = "id"
+        private const val FORMAT: String = "format"
+        private const val STATUS: String = "status"
+        private const val TITLES: String = "titles"
+        private const val DESCRIPTIONS: String = "descriptions"
+        private const val COVER_IMAGE: String = "cover_image"
+        private const val SCORE: String = "score"
+    }
+}
