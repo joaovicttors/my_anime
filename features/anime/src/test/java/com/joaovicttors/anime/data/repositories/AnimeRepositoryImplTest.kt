5cd37c0c2868b37b967ab9444b89baa6898d58b8
joaovicttors <joaovicttorrs@gmail.com>
2022-05-04 10:16:18 -0300

    add anime repository with your unit test

--- features/anime/src/test/java/com/joaovicttors/anime/data/repositories/AnimeRepositoryImplTest.kt
+++ features/anime/src/test/java/com/joaovicttors/anime/data/repositories/AnimeRepositoryImplTest.kt
@@ -2,9 +2,17 @@ package com.joaovicttors.anime.data.repositories
 
 import com.joaovicttors.anime.data.datasources.local.AnimeLocalDataSource
 import com.joaovicttors.anime.data.datasources.remote.AnimeRemoteDataSource
+import com.joaovicttors.anime.domain.entities.Anime
 import com.joaovicttors.anime.domain.repositories.AnimeRepository
+import com.joaovicttors.base.Response
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
+import org.junit.Test
 
 internal class AnimeRepositoryImplTest {
 
@@ -14,8 +22,84 @@ internal class AnimeRepositoryImplTest {
 
     @Before
     fun before() {
-        localDataSource = mockk()
-        remoteDataSource = mockk()
+        localDataSource = mockk(relaxed = true)
+        remoteDataSource = mockk(relaxed = true)
         repository = AnimeRepositoryImpl(localDataSource, remoteDataSource)
     }
+
+    @Test
+    fun `when getAnimeListFromLocalStorage called it should getAnimeList from local data source`() =
+        runBlocking { ->
+            repository.getAnimeListFromLocalStorage()
+
+            coVerify { localDataSource.getAnimeList() }
+        }
+
+    @Test
+    fun `when localDataSource returns error response should getAnimeListFromLocalStorage returns error response`() =
+        runBlocking { ->
+            val expectedErrorMessage = "error"
+
+            coEvery { localDataSource.getAnimeList() } returns Response.Error(expectedErrorMessage)
+
+            val actualResponse = repository.getAnimeListFromLocalStorage()
+
+            assertTrue(actualResponse is Response.Error)
+            assertEquals(expectedErrorMessage, (actualResponse as Response.Error).message)
+        }
+
+    @Test
+    fun `when localDataSource returns success response should getAnimeListFromLocalStorage returns success response`() =
+        runBlocking { ->
+            val expectedAnimeList = listOf(mockk<Anime>())
+
+            coEvery { localDataSource.getAnimeList() } returns Response.Success(expectedAnimeList)
+
+            val actualResponse = repository.getAnimeListFromLocalStorage()
+
+            assertTrue(actualResponse is Response.Success)
+            assertEquals(expectedAnimeList, (actualResponse as Response.Success).data)
+        }
+
+    @Test
+    fun `when getAnimeListFromRemoteStorage called it should getAnimeList from remote data source`() =
+        runBlocking { ->
+            repository.getAnimeListFromRemoteStorage()
+
+            coVerify { remoteDataSource.getAnimeList() }
+        }
+
+    @Test
+    fun `when localDataSource returns error response should getAnimeListFromRemoteStorage returns error response`() =
+        runBlocking { ->
+            val expectedErrorMessage = "error"
+
+            coEvery { remoteDataSource.getAnimeList() } returns Response.Error(expectedErrorMessage)
+
+            val actualResponse = repository.getAnimeListFromRemoteStorage()
+
+            assertTrue(actualResponse is Response.Error)
+            assertEquals(expectedErrorMessage, (actualResponse as Response.Error).message)
+        }
+
+    @Test
+    fun `when localDataSource returns success response should getAnimeListFromRemoteStorage returns success response`() =
+        runBlocking { ->
+            val expectedAnimeList = listOf(mockk<Anime>())
+
+            coEvery { remoteDataSource.getAnimeList() } returns Response.Success(expectedAnimeList)
+
+            val actualResponse = repository.getAnimeListFromRemoteStorage()
+
+            assertTrue(actualResponse is Response.Success)
+            assertEquals(expectedAnimeList, (actualResponse as Response.Success).data)
+        }
+
+    @Test
+    fun `when insertAnimeListOnLocalStorage called it should insertAnimeList on local data source`() =
+        runBlocking { ->
+            repository.insertAnimeListOnLocalStorage(emptyList())
+
+            coVerify { localDataSource.insertAnimeList(any()) }
+        }
 }
