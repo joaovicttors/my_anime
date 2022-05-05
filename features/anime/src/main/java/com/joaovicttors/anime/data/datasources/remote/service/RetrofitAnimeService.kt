678e4a5940d3b6acce4fc2dc8fcec260f29d92cf
joaovicttors <joaovicttorrs@gmail.com>
2022-05-04 22:29:47 -0300

    add retrofit annotation to service

--- features/anime/src/main/java/com/joaovicttors/anime/data/datasources/remote/service/RetrofitAnimeService.kt
+++ features/anime/src/main/java/com/joaovicttors/anime/data/datasources/remote/service/RetrofitAnimeService.kt
@@ -1,8 +1,14 @@
 package com.joaovicttors.anime.data.datasources.remote.service
 
 import com.joaovicttors.anime.data.models.AnimeRemoteModel
+import retrofit2.http.GET
 
 interface RetrofitAnimeService {
 
+    @GET(ANIME_LIST_ENDPOINT)
     suspend fun getAnimeList(): AnimeRemoteModel
+
+    companion object {
+        const val ANIME_LIST_ENDPOINT: String = "v1/anime"
+    }
 }
