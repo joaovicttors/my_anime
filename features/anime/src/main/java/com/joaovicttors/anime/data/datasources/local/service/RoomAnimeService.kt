196c4d85753fcc6d37725dab71941e1ea981a0fc
joaovicttors <joaovicttorrs@gmail.com>
2022-05-04 22:26:43 -0300

    add room annotation to dao

--- features/anime/src/main/java/com/joaovicttors/anime/data/datasources/local/service/RoomAnimeService.kt
+++ features/anime/src/main/java/com/joaovicttors/anime/data/datasources/local/service/RoomAnimeService.kt
@@ -1,10 +1,17 @@
 package com.joaovicttors.anime.data.datasources.local.service
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.joaovicttors.anime.data.models.AnimeLocalModel
 
+@Dao
 interface RoomAnimeService {
 
+    @Query(value = "SELECT * FROM anime")
     suspend fun getAll(): List<AnimeLocalModel>
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAll(animeList: List<AnimeLocalModel>)
 }
